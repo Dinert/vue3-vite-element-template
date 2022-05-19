c685d3151f63caa70fec1f54a0e1f6db93978817
Dinert <1293437362@qq.com>
2022-05-19 19:54:10 +0800

    分页切换完成

---
+++ src/views/Main/Attendance/config/form.js
@@ -0,0 +1,7 @@
+
+export const formItem = reactive({
+  date: {
+    type: 'date',
+    label: '日期'
+  }
+})
--- src/views/Main/Attendance/index.vue
+++ src/views/Main/Attendance/index.vue
@@ -1,12 +1,27 @@
 <script setup>
+import DForm from '@/base-ui/d-form'
+import { formItem } from './config/form'
+import { getFormValue } from '@/utils'
+import { reset } from '@/hook'
 
-
+// data
+const formValue = reactive(getFormValue(formItem))
+const defaultValue = reactive(getFormValue(formItem))
 
 </script>
 
 <template>
   <section class="attendance">
-    attendance
+    <d-form ref="searchForm" v-bind="{
+      formItem,
+      model: formValue,
+      inline: true
+    }">
+      <template #search>
+        <el-button type="primary" @click="search">查询</el-button>
+        <el-button type="default" @click="reset(formValue, defaultValue)">重置</el-button>
+      </template>
+    </d-form>
   </section>
 </template>
 
